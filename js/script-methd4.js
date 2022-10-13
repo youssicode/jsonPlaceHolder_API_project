48a2a587ae15aec2d97a9066f4d0f7fcc2adf216
youssef <youssimail@hotmail.com>
2022-10-13 20:31:53 +0200

    use XMLHttpRequest() AND Promise

--- js/script-methd4.js
+++ js/script-methd4.js
@@ -1,45 +1,42 @@
 const usersList = document.querySelector(".users-list ul")
 const postsList = document.querySelector(".posts-list")
 
-// Fetching API Data using XMLHttpRequest
-const httpReq = new XMLHttpRequest()
-const userUrl = "https://jsonplaceholder.typicode.com/users"
-httpReq.open('get', userUrl)
-httpReq.send()
-// Display the progress of the data received
-httpReq.onprogress = function (ev) {
-    if (ev.lengthComputable) {
-        postsList.innerText = `${ev.loaded} Byte of ${ev.total} Byte loaded!`
-    } else {
-        postsList.innerText = `${ev.loaded} Byte loaded!`
-    }
-}
-
-const reqResult = new Promise((resolve, reject) => {
-    httpReq.onload = function () {
-        if (httpReq.status >= 200 && httpReq.status < 300) {
-            resolve(JSON.parse(httpReq.responseText))
-        } else {
-            reject(Error(httpReq.statusText))
+// Fetching API Data using XMLHttpRequest and Promise (AJAX)
+let getData = function (url) {
+    return new Promise((resolve, reject) => {
+        const httpReq = new XMLHttpRequest()
+        httpReq.open('get', url)
+        httpReq.send()
+        // Display the progress of the data received == OPTIONAL ==
+        httpReq.onprogress = function (ev) {
+            if (ev.lengthComputable) {
+                postsList.innerText = `${ev.loaded} Byte of ${ev.total} Byte loaded!`
+            } else {
+                postsList.innerText = `${ev.loaded} Byte loaded!`
+            }
         }
-    }
-})
-
-reqResult.then((users) => {
-    // Rendering users list
-    if (users) {
-        users.forEach(function (user) {
-            const content = `
-                            <li class="user-link" id="${user.id}">
-                            ${user.name}
-                            <p>${user.email}</p>
-                            </li>
-                            `
-            usersList.insertAdjacentHTML('beforeend', content)
-            // insertAdjacentElement & insertAdjacentText won't work here
-        })
-        // postsList.innerText = "Click on a user link to display his posts.."
-    }
+        // If the request succeded, get the result
+        httpReq.onload = function () {
+            if (httpReq.status >= 200 && httpReq.status < 300) {
+                resolve(JSON.parse(httpReq.responseText))
+            } else {
+                reject(Error(httpReq.statusText))
+            }
+        }
+    })
+}
+const userUrl = "https://jsonplaceholder.typicode.com/users"
+getData(userUrl).then(function (users) {
+    users.forEach(function (user) {
+        const content = `
+                        <li class="user-link" id="${user.id}">
+                        ${user.name}
+                        <p>${user.email}</p>
+                        </li>
+                        `
+        usersList.insertAdjacentHTML('beforeend', content)
+        postsList.innerText = "Click on a user link to display his posts.."
+    })
 })
     .then(function () {
         //* rendering posts related to a specific user by Fetching URL with parameter
@@ -48,18 +45,18 @@ reqResult.then((users) => {
         lIs.forEach(link => {
             link.addEventListener('click', function () {
                 let userId = this.id // li's "id"
-                let postsUrl = 'https://jsonplaceholde.typicode.com/posts/?userId=' + userId
+                let postsUrl = 'https://jsonplaceholder.typicode.com/posts/?userId=' + userId
                 async function renderUserPosts() {
                     const response = await fetch(postsUrl)
                     const posts = await response.json()
                     postsList.innerHTML = '' // Vider le contenneur avant de le remplir denouveaux posts
                     posts.forEach(post => {
                         const article = `
-                                <div class="post">
-                                <h3 class="post-title">${post.title} (Post id: ${post.id}) (user id: ${post.userId})</h3>
-                                <p class="post-content">${post.body}</p>
-                                </div>
-                                `
+                        <div class="post">
+                        <h3 class="post-title">${post.title} (Post id: ${post.id}) (user id: ${post.userId})</h3>
+                        <p class="post-content">${post.body}</p>
+                        </div>
+                        `
                         postsList.insertAdjacentHTML('beforeend', article)
                     })
                 }
